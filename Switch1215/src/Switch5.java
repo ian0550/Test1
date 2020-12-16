40c7d5f0383324fced84bb78de758cb4615b2eb7
ian0550 <2_wish@naver.com>
2020-12-16 09:12:15 +0900

    201216

---
+++ Switch1215/src/Switch5.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+
+public class Switch5 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("-------------------------------");
+		System.out.println("식당 메뉴 자동 주문 프로그램 v1.0");
+		System.out.println("-------------------------------");
+		System.out.println("원하는 식사를 선택하세요.\n");
+		System.out.println("a. 김치찌개 : 5,000원");
+		System.out.println("b. 된장찌개 : 5,000원");
+		System.out.println("c. 칼국수  : 4,500원");
+		System.out.println("d. 잔치국수 : 4,000원");
+		System.out.println("e. 비빔밥  : 5,000원 \n");
+		
+		System.out.println("메뉴를 선택 하세요 :");
+		char menu = (char)System.in.read();
+		
+		switch (menu) {
+		case 'a' :
+			System.out.println("김치찌개를 주문했습니다.");
+			break;
+		case 'b':
+			System.out.println("된장찌개를 주문했습니다.");
+			break;
+		case 'c':
+			System.out.println("된장찌개를 주문했습니다.");
+			break;
+		case 'd':
+			System.out.println("된장찌개를 주문했습니다.");
+			break;
+		case 'e':
+			System.out.println("된장찌개를 주문했습니다.");
+			break;
+		default :
+			break;
+		}
+	}
+
+}
---
+++ java/Switch1215/src/Switch7.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Switch7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-------------------------------");
+		System.out.println("담배 자판기 프로그램 v1.0");
+		System.out.println("-------------------------------");
+		System.out.println("원하는 음료수를 메뉴에서 선택하세요.\n");
+		System.out.println("1. 에쎄 골드 리프   6,000원");
+		System.out.println("2. 에쎄 스페셜 골드  5,000원");
+		System.out.println("3. 더원 블루       4,500원");
+		System.out.println("4. 더원 오렌지     4,500원");
+		System.out.println("5. 더원 화이트     4,500원 \n");
+		System.out.println("-------------------------------");
+		System.out.print("담배를 선택 하세요 =>");
+		Scanner s = new Scanner(System.in);
+		int smoke = s.nextInt();
+		
+		String smoke1 = null;
+		int money = 0;
+		switch (smoke) {
+		case 1 :
+			System.out.println("에쎄 골드 리프를 선택하셨습니다.");
+			smoke1 = "에쎄 골드 리프";
+			money = 6000;
+			break;
+		case 2:
+			System.out.println("에쎄 스페셜 골드를 선택하셨습니다.");
+			smoke1 = "에쎄 스페셜 골드";
+			money = 5000;
+			break;
+		case 3:
+			System.out.println("더원 블루를 선택하셨습니다.");
+			smoke1 = "더원 블루";
+			money = 4500;
+			break;
+		case 4:
+			System.out.println("더원 오렌지를 선택하셨습니다.");
+			smoke1 = "더원 오렌지";
+			money = 4500;
+			break;
+		case 5:
+			System.out.println("더원 화이트를 선택하셨습니다.");
+			smoke1 = "더원 화이트";
+			money = 4500;
+			break;
+		default :
+			break;
+		}
+		System.out.println("-------------------------------");
+		System.out.println("담배 금액을 입금하세요 =>");
+		int money1 = s.nextInt();
+		System.out.printf("%s를 구매했습니다. 거스름돈 %d원을 받으세요.\n", smoke1, (int)(money1 - money));
+		s.close();
+	}
+
+}
