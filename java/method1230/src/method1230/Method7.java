187eaec3fb0503a84f3b9ba2c1c79973a13c796f
ian0550 <2_wish@naver.com>
2020-12-31 13:10:42 +0900

    1231_모의고사

--- java/method1230/src/method1230/Method7.java
+++ java/method1230/src/method1230/Method7.java
@@ -46,60 +46,83 @@ public class Method7 {
 
 	public static void gugu2() {
 		for (int i = 1; i < 10; i++) {
-				for (int j = 2; j < 10; j++) {
-					if (j % 2 != 0) {
+			for (int j = 2; j < 10; j++) {
+				if (j % 2 != 0) {
 					System.out.printf("%d X %d = %d \t", j, i, (i * j));
-					}
 				}
-				System.out.println();
+			}
+			System.out.println();
 		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		1.3.4.5.8
-		int a = (int) (Math.random() * 100) + 1;
-		int b = (int) (Math.random() * 100) + 1;
-		int c = (int) (Math.random() * 100) + 1;
-		int d = (int) (Math.random() * 100) + 1;
-		System.out.println("1번문제-----------------");
-		System.out.printf(" %d, %d, %d, %d -> ", a, b, c, d);
-		int arr[] = new int[] { a, b, c, d };
-		sort(arr);
-
-		System.out.println("\n\n3번문제------------------");
-		for (int i = 0; i < 10; i++) {
-			a = (int) (Math.random() * 100) + 1;
-			System.out.println((i + 1) + "번째 = " + a);
-			HolJJack(a);
-		}
-
-		System.out.println("\n\n4번문제------------------");
-		int kuk = (int) (Math.random() * 100) + 1;
-		int eng = (int) (Math.random() * 100) + 1;
-		int mat = (int) (Math.random() * 100) + 1;
-		System.out.printf("국어: %d,영어: %d,수학: %d \n", kuk, eng, mat);
-		jumsu(kuk, eng, mat);
-
-		System.out.println("\n\n5번문제------------------");
-		a = (int) (Math.random() * 9) + 1;
-		gugu(a);
-
-		System.out.println("\n\n8번문제------------------");
-		gugu2();
+//		int a = (int) (Math.random() * 100) + 1;
+//		int b = (int) (Math.random() * 100) + 1;
+//		int c = (int) (Math.random() * 100) + 1;
+//		int d = (int) (Math.random() * 100) + 1;
+//		System.out.println("1번문제-----------------");
+//		System.out.printf(" %d, %d, %d, %d -> ", a, b, c, d);
+//		int arr[] = new int[] { a, b, c, d };
+//		sort(arr);
+//
+//		System.out.println("\n\n3번문제------------------");
+//		for (int i = 0; i < 10; i++) {
+//			a = (int) (Math.random() * 100) + 1;
+//			System.out.println((i + 1) + "번째 = " + a);
+//			HolJJack(a);
+//		}
+//
+//		System.out.println("\n\n4번문제------------------");
+//		int kuk = (int) (Math.random() * 100) + 1;
+//		int eng = (int) (Math.random() * 100) + 1;
+//		int mat = (int) (Math.random() * 100) + 1;
+//		System.out.printf("국어: %d,영어: %d,수학: %d \n", kuk, eng, mat);
+//		jumsu(kuk, eng, mat);
+//
+//		System.out.println("\n\n5번문제------------------");
+//		a = (int) (Math.random() * 9) + 1;
+//		gugu(a);
+//
+//		System.out.println("\n\n8번문제------------------");
+//		gugu2();
+//		
+//		short num1 = 22;
+//       short num2 = 11;
+//       int result = num1+num2;
+//       System.out.println(result);
+//       int n = 5;
+//		for (int i = 0; i < n; i++) {
+//			for (int j = n-i-1; j >0  ; j--) {
+//				System.out.printf("-");
+//			}
+//			for (int k = 0; k < i*2+1; k++) {
+//				System.out.printf("*");
+//			}
+//			System.out.println();
+//		}
+		int hap=0;
+//		for (int i = 0; i < 100; i++) {
+//			if(i%2 != 0) {
+//				hap += i;
+//			}
+//		}
+//		System.out.println(hap);
+		Scanner s = new Scanner(System.in);
+		System.out.println("첫번째 숫자입력 : ");
+		int num1 = s.nextInt();
+		System.out.println("두번째 숫자입력 : ");
+		int num2 = s.nextInt();
 		
-		short num1 = 22;
-       short num2 = 11;
-       int result = num1+num2;
-       System.out.println(result);
-       
-       for (int i = 2; i < 10; i++) {
-			for (int j = 1; j < 10; j++) {
-				System.out.printf("%d X %d = %d \n", i, j, (i * j));
-				}
+		for (int i = 0; i < num1; i++) {
+			if(i%num2 == 0) {
+				hap += i;
+			}
 		}
-	
-       
+		System.out.println(hap);
+		
+		
 	}
 
 }
