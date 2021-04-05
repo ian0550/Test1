89dc471a9697b8b0d216530c85a6d85a8dfadd97
ian0550 <2_wish@naver.com>
2021-04-05 16:42:30 +0900

    0405

---
+++ csharp/210405/210405/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _210405
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("WriteWriteLine" + Environment.NewLine);
+            //모든 운영체제에서 적용 가능한 줄바꿈
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteWrite");
+            Console.WriteLine("\"Write\"");
+            Console.WriteLine("\"\\\"Write\\\"\"");//escape 문자
+
+            Console.WriteLine(1 / 2.0);
+            Console.WriteLine(1.0 / 2);
+
+
+            Console.WriteLine(5 + 3);
+            Console.WriteLine(5 - 3);
+            Console.WriteLine(5 * 3);
+            Console.WriteLine(5 / 3);
+            Console.WriteLine(5 % 3);
+
+            Console.WriteLine(-4 % 3);
+            Console.WriteLine(4 % -3);
+
+
+            Console.WriteLine("안녕하세요"[0]);//안
+            Console.WriteLine("안녕하세요"[4]);//요
+            // Console.WriteLine("안녕하세요"[5]);//error
+            Console.WriteLine((int)"안녕하세요"[0]);//안
+            Console.WriteLine(Convert.ToString((int)"안녕하세요"[0], 16));
+
+
+            Console.WriteLine(1 == 1);
+            Console.WriteLine(1 != 1);
+            bool result = true;
+            bool result2 = false;
+
+            Console.WriteLine(!result); //논리 부정 연산자
+            Console.WriteLine(!result2);
+            Console.WriteLine(result || result2);
+            Console.WriteLine(result && result2);
+            Console.WriteLine(result2 || result2);
+
+
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(int.MaxValue);
+
+            int a = int.MaxValue; //2147483647
+            int b = int.MaxValue;
+            Console.WriteLine(a + b);
+            Console.WriteLine((long)a + b);
+
+            uint c = uint.MaxValue;
+            Console.WriteLine(c);
+
+            int conclusion = 100;
+            conclusion++;
+            Console.WriteLine(conclusion);
+            conclusion++;
+            Console.WriteLine(conclusion);
+
+            string input = Console.ReadLine();
+            Console.WriteLine("안녕 나는 " + input + "이야");
+
+            int number = int.Parse(Console.ReadLine()); //정수
+            double num = double.Parse(Console.ReadLine()); //실수
+            Console.WriteLine(number/num);
+
+        }
+    }
+}
---
+++ csharp/210405_practice/210405_practice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _210405_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("cm로 변환 할 inch를 입력해주세요");
+            //double inch = double.Parse(Console.ReadLine());
+            //Console.WriteLine(inch + "는 " + (inch*2.54) + "cm 입니다.\n");
+
+            //Console.WriteLine("pound로 변환 할 kg를 입력해주세요");
+            //double pound = double.Parse(Console.ReadLine());
+            //Console.WriteLine(pound + "는 " + (pound * 2.20462262) + "cm 입니다.\n");
+
+            //Console.WriteLine("원의 반지름을 입력해주세요");
+            //double radii = double.Parse(Console.ReadLine());
+            //Console.WriteLine("원 둘레는 " + (radii*3.14*2) + "cm 입니다.");
+            //Console.WriteLine("원 넓이는 " + (3.14 * radii * radii) + "입니다.");
+
+            Console.WriteLine("숫자를 2개 입력해주세요");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int cal1, cal2, cal3;
+
+            Console.WriteLine("1." + a);
+            Console.WriteLine("2." + b);
+            Console.WriteLine("3." + b % 10 * a);
+            Console.WriteLine("4." + b / 10 % 10 * a);
+            Console.WriteLine("5." + b / 100 * a);
+
+
+
+        }
+    }
+}
