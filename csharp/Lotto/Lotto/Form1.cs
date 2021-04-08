16ec889f2afd86a45752540185755f9a5ba54b31
ian0550 <2_wish@naver.com>
2021-04-08 16:39:49 +0900

    0408_lotte,개미,별,설탕배달

---
+++ csharp/Lotto/Lotto/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lotto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] num = new int[7];
+            Random r = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                num[i] = r.Next(1, 7);
+                for (int j = 1; j < i; j++)
+                {
+                    if (num[i] == num[j])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            label_num1.Text = num[1].ToString();
+            label_num2.Text = num[2].ToString();
+            label_num3.Text = num[3].ToString();
+            label_num4.Text = num[4].ToString();
+            label_num5.Text = num[5].ToString();
+            label_num6.Text = num[6].ToString();
+        }
+    }
+}
---
+++ csharp/yester_answer/answer/answer/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace answer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. 5개의 숫자 중에 최댓값, 최솟값 구하기
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int min = numbers[0];
+            int max = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                    min = numbers[i];
+                if (numbers[i] > max)
+                    max = numbers[i];
+            }
+            Console.WriteLine("min : " + min + " max : " + max);
+            // 2. 읽고 말하기 수열(개미수열)
+            string start = "1";
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine((i+1)+"번째 수열:" + start);
+                char number = start[0];
+                int count = 0;
+                string end = "";
+
+                for (int j = 0; j < start.Length; j++)
+                {
+                    if (number==start[j])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        //1121 1에서 2로 넘어가는 시점 등
+                        end = end + number + count; //1이 2개
+                        number = start[j];
+                        count = 1;
+                    }
+                }
+                end = end + number + count;
+                start = end;
+            }
+
+            // 3. 별찍기
+            Console.WriteLine("3번");
+            int inputFloor = int.Parse(Console.ReadLine());
+            int flag = 0;
+            if(inputFloor == 1)
+                Console.WriteLine(("*"));
+            else
+            {
+                for (int i = 0; i < inputFloor*2; i++)
+                {
+                    for (int j = 0; j < inputFloor; j++)
+                    {
+                        if(flag%2 == 0)
+                            Console.Write("*");
+                        else
+                            Console.Write(" ");
+                        flag++;
+                    }
+                    if (inputFloor % 2 == 0)
+                        flag++;
+                    Console.WriteLine();
+                }
+            }
+            // 4. 설탕 배달 문제
+            Console.WriteLine("4번");
+            int totalSugar = int.Parse(Console.ReadLine());
+            int podae = 0;
+            while (true)
+            {
+                if (totalSugar%5 == 0)
+                {
+                    podae += totalSugar / 5;
+                    break;
+                }
+                totalSugar -= 3;
+                podae++;
+                if (totalSugar < 0)
+                {
+                    podae = -1;
+                    break;
+                }
+            }
+            Console.WriteLine(podae);
+        }
+    }
+}
